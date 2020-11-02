e76f1f72723e9eadd5e19fdca6d94d6c10a232d5
Andrew Smirnov <sobachylie@gmail.com>
2020-11-02 16:55:36 +0300

    fix data

--- weather.js
+++
@@ -1,7 +0,0 @@
-fetch('http://api.openweathermap.org/data/2.5/weather?q=Taganrog&units=metric&lang=ru&appid=13e5b32f3d5e54970c4e2790c623939e')
-  .then((response) => {
-    return response.json();
-  })
-  .then((data) => {
-    console.log(data);
-  });
